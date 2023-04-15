a0e89bc517c45247f049ad7641d80161c334ca58
JacobSoko <128942029+JacobSoko@users.noreply.github.com>
2023-04-15 18:46:41 +0300

    Delete mainApp.java

--- mainApp.java
+++
@@ -1,6 +0,0 @@
-class mainApp{
-    public static void main(String args[]){
-        Master master = new Master(Integer.parseInt(args[0]));
-        System.out.println("The number of workers is " + master.getNumOfWorkers());    
-    }
-}    
