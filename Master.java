bad0956a78a4711550ae55cd61c68e460939ba76
JacobSoko <128942029+JacobSoko@users.noreply.github.com>
2023-04-15 18:46:19 +0300

    Add files via upload

--- Master.java
+++ Master.java
@@ -9,7 +9,6 @@ class Master{ //implements Runnable{
     private Socket socket;
     ObjectInputStream in;
     ObjectOutputStream out;
-    private int port;
 
     private ArrayList <Worker> listOfWorkers;
 
@@ -19,10 +18,14 @@ class Master{ //implements Runnable{
 
     Master(int numOfWorkers){
         this.numOfWorkers = numOfWorkers;
+        listOfWorkers = new ArrayList<Worker>();
         for (int i = 1; i <= numOfWorkers; i++){
             Worker worker = new Worker(i);
             System.out.println("Creating worker number.. " + worker.getId());
+            System.out.println(worker);
             listOfWorkers.add(worker);
+            System.out.println("eftiaksa ton ergath");
+            System.out.println(listOfWorkers.size());
         }
     }
 
@@ -30,22 +33,35 @@ class Master{ //implements Runnable{
         return this.numOfWorkers;
     }
 
+    public ArrayList<Worker> getList(){
+        return this.listOfWorkers;
+    }
+
+    
+
     void openServer(){
         try{
-            this.serverSocket = new ServerSocket(port);
+            this.serverSocket = new ServerSocket(5000);
             while (true){
                 System.out.println("Waiting for connection..");
                 this.socket = serverSocket.accept();
                 System.out.println("Worker connected");
                 this.out = new ObjectOutputStream(socket.getOutputStream());
                 this.in = new ObjectInputStream(socket.getInputStream());
+                System.out.println(in.readUTF());
+                if (!in.readUTF().isEmpty() ){
+                    break;
+                }      
                 //new Thread.run();
             }
         } catch(IOException ioException){
 			ioException.printStackTrace();
-		} finally{
+		} /*catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }*/ finally{
 			try{
 				this.serverSocket.close();
+                this.socket.close();
 			} catch(IOException ioException){
 				ioException.printStackTrace();
 			}
@@ -69,6 +85,17 @@ class Master{ //implements Runnable{
             out.close();
         } catch(IOException ioException){
             ioException.printStackTrace();
+        }
+    }
+
+    public static void main(String args[]){
+        Master master = new Master(Integer.parseInt(args[0]));
+        System.out.println("The number of workers is " + master.getNumOfWorkers());
+        master.openServer();
+        System.out.println(master.getList().size());
+        for (Worker worker : master.getList()){
+            System.out.print("mphka sto loop");
+            worker.run();
         }
     }
 }
--- Worker.java
+++ Worker.java
@@ -1,13 +1,16 @@
 import java.net.*;
+import java.util.ArrayList;
 import java.io.*;
 
-class Worker{// implements Runnable{
+class Worker implements Runnable{
 
     private int id;
+    private Socket socket;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
     //ip address;
 
     Worker(int id){
-        //super();
         this.id = id;
     }
 
@@ -15,8 +18,38 @@ class Worker{// implements Runnable{
         return this.id;
     }
 
-    @Override
-    void run(){
-        Master.reduce(key,value);
+    public String toString(){
+        return "Hi im worker " + getId();
     }
+
+    @Override
+    public void run(){
+        try{
+            this.socket = new Socket("localhost",5000);
+            //socket.connect(5000);
+            this.out = new ObjectOutputStream(socket.getOutputStream());
+            this.in = new ObjectInputStream(socket.getInputStream());
+            String message = "Hello server";
+            this.out.writeUTF(message);
+            this.out.flush();
+            System.out.println(message);
+        } catch (UnknownHostException unknownHost) {
+			System.err.println("You are trying to connect to an unknown host!");
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		} finally {
+			try {
+				in.close();	out.close();
+				socket.close();
+			} catch (IOException ioException) {
+				ioException.printStackTrace();
+			}
+		}
+    }
+
+    public static void main(String args[]){
+        Worker worker = new Worker(1);
+        worker.run();
+    }
+    
 }
