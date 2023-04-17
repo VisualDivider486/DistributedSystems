0585c93371ae8bd9c0b5afb15c6e9a139683f027
JacobSoko <128942029+JacobSoko@users.noreply.github.com>
2023-04-17 18:00:43 +0300

    Add files via upload

--- Master.java
+++ Master.java
@@ -4,29 +4,51 @@ import java.util.*;
 
 class Master{ //implements Runnable{
 
-    private static int numOfWorkers;
+    private ArrayList<Integer> ports = new ArrayList<Integer>();
     private ServerSocket serverSocket;
     private Socket socket;
+    private int numOfPorts;
     ObjectInputStream in;
     ObjectOutputStream out;
 
     Master(){
-
+        BufferedReader reader = null;
+        try{
+			reader = new BufferedReader(new FileReader("configfile.txt"));
+		}
+		catch (FileNotFoundException e ){
+			System.err.println("Error opening file!");
+        }
+        try{
+            String line = reader.readLine();
+            while (line != null){
+                if (line.trim().startsWith("PORT ")){
+                    int port = Integer.parseInt(line.trim().substring(5));
+                    ports.add(port);
+                    numOfPorts++;
+                }
+                line = reader.readLine();
+            }
+            reader.close();
+        }
+        catch (IOException e){
+            System.out.println("Error reading line ...");
+        }
     }
 
-    Master(int numOfWorkers){
-        this.numOfWorkers = numOfWorkers;
+    int getNumOfPorts(){
+        return this.numOfPorts;
     }
 
-    int getNumOfWorkers(){
-        return this.numOfWorkers;
+    ArrayList<Integer> getPorts(){
+        return this.ports;
     }
     
     void openServer(){
         try{
             this.serverSocket = new ServerSocket(5000);
             int i = 0;
-            while (i <= 3){
+            while (i < getNumOfPorts()){
                 System.out.println("Waiting for connection..");
                 this.socket = serverSocket.accept();
                 System.out.println("Worker connected");
@@ -48,6 +70,13 @@ class Master{ //implements Runnable{
 				ioException.printStackTrace();
 			}
 		} 
+    }
+
+    void printList(){
+        System.out.println(">>>>>>>Printing List..");
+        for (Integer z : ports){
+            System.out.println(z);
+        }
     }
 
     /*@Override
@@ -72,8 +101,8 @@ class Master{ //implements Runnable{
     }
 
     public static void main(String args[]){
-        Master master = new Master(4);
-        System.out.println("The number of workers is " + master.getNumOfWorkers());
+        Master master = new Master();
+        master.printList();
         master.openServer();
         master.disconnect();
     }
