f1570f9f91996a79cdcc4f33b920ba710054dd10
Konstantinos Marko <kwstasmarko2001@gmail.com>
2023-04-19 23:50:23 +0300

    Create Waypoint.java

---
+++ Waypoint.java
@@ -0,0 +1,14 @@
+class Waypoint{
+	String time;
+	float elevation;
+	double lat;
+	double longt;
+	
+	Waypoint(String time,float elevation,double lat,double longt){
+	
+		this.time=time;
+		this.elevation=elevation;
+		this.lat=lat;
+		this.longt=longt;
+	}
+}
